6290b64fd60def9847e03455c9952727a58d99e6
Tara <nandayavanets@mail.ru>
2025-06-29 19:38:50 +0700

    added add word and changed image in content panel

--- frontend/src/components/ui/ContentPanel.jsx
+++ frontend/src/components/ui/ContentPanel.jsx
@@ -3,8 +3,8 @@ export const Content = () => {
     <section className="h-full flex-1 overflow-auto">
         {/* put width full doesnt work using div bellow since it has not fixed width. Applying flex-1 doesnt either, so put the flex in section */}
       <div className="">
-        <div>
-            <img src="https://images.pexels.com/photos/1553963/pexels-photo-1553963.jpeg" alt="image" />
+        <div className="">
+            <img src="https://images.pexels.com/photos/1553963/pexels-photo-1553963.jpeg" alt="image" className="w-full object-cover" />
         </div>
       </div>
     </section>
